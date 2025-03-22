9f0503775167e2b751dbb67f3f5b2f71eeb5799f
TharHtet236401 <tharhteta3@gmail.com>
2025-03-23 02:06:53 +0700

    set up the image folder

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,HttpResponse
 from django.http import HttpResponse
-
+from .models import Product
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    products = Product.objects.all()
+    context = {
+        'products':products
+    }
+    return render(request,'home.html',context)
