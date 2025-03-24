7394d00a187aa85353a369bb889950f06309530a
TharHtet236401 <tharhteta3@gmail.com>
2025-03-24 15:58:59 +0700

    added the register UI

--- store/views.py
+++ store/views.py
@@ -15,6 +15,10 @@ def home(request):
 def about(request):
     return render(request,'about.html')
 
+
+def register_user(request):
+    return render(request,'register.html')
+
 def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
