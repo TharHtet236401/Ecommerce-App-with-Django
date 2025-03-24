b93f66b02bb707d9504633d6ec1aaf9b1644079e
TharHtet236401 <tharhteta3@gmail.com>
2025-03-24 15:07:47 +0700

    log in page added

--- store/views.py
+++ store/views.py
@@ -13,3 +13,11 @@ def home(request):
 def about(request):
     return render(request,'about.html')
 
+def login_user(request):
+    return render(request,'login.html')
+
+
+def logout_user(request):
+    return HttpResponse("Logout")
+
+
