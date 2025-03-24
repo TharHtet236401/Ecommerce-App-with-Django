54f2ac65ab02831dd46149c9f1c55250a1ba9b83
TharHtet236401 <tharhteta3@gmail.com>
2025-03-24 15:50:52 +0700

    beautify the UI of the login form

--- store/views.py
+++ store/views.py
@@ -22,6 +22,7 @@ def login_user(request):
         user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
+            messages.success(request,'You have been logged in')
             return redirect('home')
         else:
             messages.error(request,'Invalid username or password')
