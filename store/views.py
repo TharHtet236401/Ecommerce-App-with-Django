4e7d03b93f0117d733e0c203d5235c5ff5808997
TharHtet236401 <tharhteta3@gmail.com>
2025-03-24 15:40:26 +0700

    log in log out function works

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.http import HttpResponse
 from .models import Product
 from django.contrib.auth import authenticate,login,logout
@@ -16,10 +16,22 @@ def about(request):
     return render(request,'about.html')
 
 def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid username or password')
+            return redirect('login')
     return render(request,'login.html')
 
 
 def logout_user(request):
-    return HttpResponse("Logout")
+    logout(request)
+    messages.success(request,'You have been logged out')
+    return redirect('login')
 
 
