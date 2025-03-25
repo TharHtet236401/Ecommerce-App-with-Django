e1f191b1c29227fd01aea4c5c77d176c7a0b8d1b
TharHtet236401 <tharhteta3@gmail.com>
2025-03-25 15:15:28 +0700

    finihsed cart_setup

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
+
+def cart_summary(request):
+    return render(request,'cart_summary.html')
+
+def cart_add(request):
+    pass
+
+def cart_update(request):
+    pass
+
+def cart_delete(request):
+    pass
+
+
